ca3caf166f03dc446801b791d1062f181e3b3bfe
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-03-03 21:45:05 +0800

    initial commit

---
+++ Content/test.ss
@@ -0,0 +1,18 @@
+﻿i = 1;
+k = i + 2.1;
+name = 'Sean';
+
+j = i + 1;
+
+while i < 8
+{
+    Write("i isnt big enough - " + i + " so lets make it bigger!");
+	 
+    i = i + 1 * 4; 
+}
+else
+{
+    Write("i is good enough");
+}
+
+Write("i is " + i + ".");
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Antlr4.Runtime;
+using ConsoleApp1;
+using ConsoleApp1.Content;
+
+var filename = "Content\\test.ss";
+
+var fileContents = File.ReadAllText(filename);
+
+var inputStream = new AntlrInputStream(fileContents);
+
+var simpleLexer = new SimpleLexer(inputStream);
+
+CommonTokenStream commonTokenStream = new CommonTokenStream(simpleLexer);
+
+var simpleParser = new SimpleParser(commonTokenStream);
+
+simpleParser.AddErrorListener(new DiagnosticErrorListener());
+
+var simpleContext = simpleParser.program();
+
+var visitor = new SimpleVisitor();
+
+visitor.Visit(simpleContext);
